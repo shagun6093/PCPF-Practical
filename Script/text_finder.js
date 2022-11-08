36bd515f491e9252fa83f344153fa1d455bd60bb
Divya Shah <102017379+divyashah0510@users.noreply.github.com>
2022-11-08 15:32:56 +0530

    Added Folder Script

---
+++ Script/text_finder.js
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>JavaScript</h1>
+<script>
+let string1=prompt("Enter a string: ");
+let string2=prompt("Enter the text to be searched:");
+let result = string1.includes(string2);
+alert("Result is : "+result);
+</script>
+</body>
+</html>
