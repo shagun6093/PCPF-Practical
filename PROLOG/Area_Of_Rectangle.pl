d3f6415540099c9e90d76a5155234d47e93cea1b
Shagun Gupta <102024729+shagun6093@users.noreply.github.com>
2022-11-08 13:50:41 +0530

    Create Area_Of_Rectangle.pl

---
+++ PROLOG/Area_Of_Rectangle.pl
@@ -0,0 +1,10 @@
+go:-
+    write('Enter the Length and breadth of rectangle'),nl,
+    read(Breadth),
+    read(Length),
+    Area is (Breadth*Length),nl,
+    write('Area of Rectangle:'),
+    write(Area).
+
+% output
+% ?-go.
