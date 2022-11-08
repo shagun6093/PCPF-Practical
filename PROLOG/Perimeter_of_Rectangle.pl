6021006b8aef8b5f0c49132fbd9111a98bd8d2fa
Shagun Gupta <102024729+shagun6093@users.noreply.github.com>
2022-11-08 13:53:31 +0530

    Create Perimeter_of_Rectangle.pl

---
+++ PROLOG/Perimeter_of_Rectangle.pl
@@ -0,0 +1,10 @@
+go:-
+    write('Enter the Length and breadth of rectangle'),nl,
+    read(Breadth),
+    read(Length),
+    Perimeter is (2*(Breadth + Length)),nl,
+    write('Perimeter of Rectangle:'),
+    write(Perimeter).
+
+% output
+% ?-go.
