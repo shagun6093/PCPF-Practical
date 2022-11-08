ebd7feb829d5ebc748413c64b62e1a2d12fb1e08
Shagun Gupta <102024729+shagun6093@users.noreply.github.com>
2022-11-08 13:22:27 +0530

    Create maximum.pl

---
+++ PROLOG/maximum.pl
@@ -0,0 +1,9 @@
+max_of_two(X,Y,X) :- X >= Y.
+max_of_two(X,Y,Y) :- X < Y.
+list_max_elem([X],X).
+list_max_elem([X,Y|Rest],Max) :-
+   list_max_elem([Y|Rest],MaxRest),
+   max_of_two(X,MaxRest,Max).
+
+% output
+% ?- list_max_elem([8,5,3,4,7,9,6,1],Max).
