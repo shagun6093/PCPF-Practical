eb643b2f888e8e2426aeaeacd540a3135d43d9e8
Divya Shah <102017379+divyashah0510@users.noreply.github.com>
2022-11-08 13:30:43 +0530

    Create Surface_area_Of_sphere.pl

---
+++ PROLOG/Surface_area_Of_sphere.pl
@@ -0,0 +1,15 @@
+go:-
+    write('Enter the radius of sphere'),nl,
+    read(Radius),
+    Surface_area is (4*3.1452*Radius*Radius),nl,
+    write('Surface area of sphere is :'),
+    write(Surface_area).
+
+
+
+%Output:-
+?- sphere.
+Enter the radius of sphere
+|: 32.
+
+Surface area of sphere is :12882.7392
